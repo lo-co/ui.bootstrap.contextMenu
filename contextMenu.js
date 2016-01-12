7e9d4e31bac3aa01f2171b05ebe680dfca54229a
Matt Richardson <matt.richardson@msrconsults.com>
2016-01-11 21:35:35 -0700

    GOOD VERSION OF CMs.

--- contextMenu.js
+++ contextMenu.js
@@ -18,7 +18,7 @@
 
                 el.css('visibility', 'visible');
 
-                var ul = element.find("ul");
+                var ul = element.find("ul.context-menu.dropdown-menu");
 
                 var left = event.pageX - el.offset().left - 5;
                 var top = event.pageY - el.offset().top - 5;
@@ -41,11 +41,11 @@
 
         var t = ['<div class="dropdown clearfix" style="visibility:hidden; position:absolute;display:inline-block" >',
             '<ul class="dropdown-menu context-menu" role "menu" style="display:block;position:absolute;">',
-            '<li ng-class="{\'dropdown-submenu\':opt[2] !== undefined}" ng-repeat="opt in menuOptions" ng-click="opt[1]()" class="menu-element" style="padding:5px" >',
-            '<div ng-switch on="opt[2] === undefined">',
-            '<div ng-switch-when=true><span ng-bind-html ="opt[0]"></span></div>',
+            '<li ng-class="{\'dropdown-submenu\':opt[2] !== undefined,  \'divider\':opt === null, \'menu-element\':opt !== null}" ng-repeat="opt in menuOptions" ng-click="opt[1]()" >',
+            '<div ng-switch on="opt[2] === undefined" ng-if="opt !== null">',
+            '<div ng-switch-when=true><div ng-bind-html ="opt[0]"></div></div>',
             '<div ng-switch-when=false><span ng-bind-html="opt[0]"></span>',
-            '<ul class="dropdown-menu"><li ng-repeat="el in opt[2]">{{el}}</li></ul>',
+            '<ul class="dropdown-menu"><li class="menu-element" ng-repeat="el in opt[2]" ng-bind-html="el[0]" ng-click="el[1]()"></li></ul>',
             '</div>',
             '</div></li>',
             '</ul></div><div class="main" ng-transclude="main"></div> '];
